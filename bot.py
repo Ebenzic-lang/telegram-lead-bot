6bf96b2e84b13af1a0c5ef264d79c5a13c1024bd
Ebenzic-lang <adeboyeebenezer2000@gmail.com>
2026-04-29 14:42:22 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,19 @@
-import asyncio
-from aiogram import Bot, Dispatcher
-from config import BOT_TOKEN
-from handlers.start import router
+from telegram.ext import Application, CommandHandler
+from handlers.start import start_handler
 
-async def main():
-    bot = Bot(token=BOT_TOKEN)
-    dp = Dispatcher()
+# Replace with your real Telegram bot token
+BOT_TOKEN = "8585594114:AAHwDuRtq1fyhNa_ntNF0-9NNMyI7EliTEA"
 
-    dp.include_router(router)
 
-    await dp.start_polling(bot)
+def main():
+    app = Application.builder().token(BOT_TOKEN).build()
+
+    # Register handlers
+    app.add_handler(start_handler)
+
+    print("Bot is running...")
+    app.run_polling()
+
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    main()
